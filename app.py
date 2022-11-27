8adefcf36e780dab7c3d7fd1c5db0da11694e814
samtha83 <69811190+samtha83@users.noreply.github.com>
2022-11-27 12:14:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ streamlit.header('Pick a sweatsuit color or style')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-CURRENT_REGION()")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
